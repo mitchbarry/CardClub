4542cee79adc53b0977a3fe871cc0a21d35eb427
Mitch Barry <140218862+mitchbarry@users.noreply.github.com>
2024-03-07 11:35:38 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Container from './components/Container';
+
+function App() {
+
+  return (
+    <div id='app'>
+      <Container />
+    </div>
+  )
+}
+
+export default App;
